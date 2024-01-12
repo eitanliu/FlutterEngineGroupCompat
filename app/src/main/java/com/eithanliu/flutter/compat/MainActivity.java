f5a6132aa6371133fa7204071e7b6f450e18ca97
eitanliu <eitanliu@gmail.com>
2024-01-13 02:56:57 +0800

    update example

--- app/src/main/java/com/eithanliu/flutter/compat/MainActivity.java
+++ app/src/main/java/com/eithanliu/flutter/compat/MainActivity.java
@@ -1,6 +1,28 @@
 package com.eithanliu.flutter.compat;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterActivityCompat;
 
 public class MainActivity extends FlutterActivityCompat {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button button = new Button(this);
+        button.setText("Next");
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+        ViewGroup.MarginLayoutParams layoutParams = new ViewGroup.MarginLayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.topMargin = 150;
+        addContentView(button, layoutParams);
+    }
 }
